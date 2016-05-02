1085dbbf0ccc4c7d25130ea868bda5d8aea937ea
向炜 <willxiang@qq.com>
2016-05-02 22:51:20 +0800

    commit local file

---
+++ js/my_weather.js
@@ -0,0 +1,16 @@
+var app = angular.module('myweather', []);
+app.controller('listCtrl', function($scope) {
+    $scope.items = [
+        { "username": "willxiang1", "age": 21, "gender": "male" },
+        { "username": "willxiang1", "age": 21, "gender": "male" },
+        { "username": "willxiang1", "age": 21, "gender": "male" },
+        { "username": "willxiang1", "age": 21, "gender": "male" },
+        { "username": "willxiang1", "age": 21, "gender": "male" },
+        { "username": "willxiang1", "age": 21, "gender": "male" },
+        { "username": "willxiang1", "age": 21, "gender": "male" },
+        { "username": "willxiang1", "age": 21, "gender": "male" },
+        { "username": "willxiang1", "age": 21, "gender": "male" },
+        { "username": "willxiang1", "age": 21, "gender": "male" }
+    ];
+
+});
