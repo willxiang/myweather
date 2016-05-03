28ab32356c6f33ae755c503b7b8eaffa72af1480
向炜 <willxiang@qq.com>
2016-05-03 10:44:42 +0800

    增加了一些代码

--- js/my_weather.js
+++ js/my_weather.js
@@ -1,16 +1,54 @@
 var app = angular.module('myweather', []);
-app.controller('listCtrl', function($scope) {
-    $scope.items = [
-        { "username": "willxiang1", "age": 21, "gender": "male" },
-        { "username": "willxiang1", "age": 21, "gender": "male" },
-        { "username": "willxiang1", "age": 21, "gender": "male" },
-        { "username": "willxiang1", "age": 21, "gender": "male" },
-        { "username": "willxiang1", "age": 21, "gender": "male" },
-        { "username": "willxiang1", "age": 21, "gender": "male" },
-        { "username": "willxiang1", "age": 21, "gender": "male" },
-        { "username": "willxiang1", "age": 21, "gender": "male" },
-        { "username": "willxiang1", "age": 21, "gender": "male" },
-        { "username": "willxiang1", "age": 21, "gender": "male" }
-    ];
+app.controller('listCtrl', function($scope, $http) {
+    // $scope.items = [
+    //     { "username": "willxiang1", "age": 21, "gender": "male" },
+    //     { "username": "willxiang1", "age": 21, "gender": "male" },
+    //     { "username": "willxiang1", "age": 21, "gender": "male" },
+    //     { "username": "willxiang1", "age": 21, "gender": "male" },
+    //     { "username": "willxiang1", "age": 21, "gender": "male" },
+    //     { "username": "willxiang1", "age": 21, "gender": "male" },
+    //     { "username": "willxiang1", "age": 21, "gender": "male" },
+    //     { "username": "willxiang1", "age": 21, "gender": "male" },
+    //     { "username": "willxiang1", "age": 21, "gender": "male" },
+    //     { "username": "willxiang1", "age": 21, "gender": "male" }
+    // ];
+
+    // var data = $scope.items[0];
+
+    // chrome.storage.sync.set({'items':"beijing"},function(){
+    //     console.log("saved");
+    // });
+
+
+    var qiniu = 'http://7xnnc6.com1.z0.glb.clouddn.com/allchina.json';
+
+    if (!$scope.items || $scope.items.length == 0) {
+        $http({
+            method: 'GET',
+            url: qiniu
+        }).then(function(response) {
+            console.log(response.data.city_info); //把数据按照键值对处理后放到本地
+            $scope.items = response.data.city_info;
+        }, function(response) {
+            console.log(response);
+        });
+    }
+
+
+
+
+    // var url = "https://api.heweather.com/x3/weather?cityid=CN101010100&key=6a7d5f6938da427fbb17219e1461e4e4";
+    // $http({
+    //     method: 'GET',
+    //     url: url
+    // }).then(function(response) {
+    //     var weather = response.data;
+    //     console.log(weather['HeWeather data service 3.0'][0]);
+    // }, function(data) {
+    //     console.log(data);
+    // });
+
+
+
 
 });
---
+++ js/request.js
@@ -0,0 +1,10 @@
+function httpRequest(url, callback){
+    var xhr = new XMLHttpRequest();
+    xhr.open("GET", url, true);
+    xhr.onreadystatechange = function() {
+        if (xhr.readyState == 4) {
+            callback(xhr.responseText);
+        }
+    }
+    xhr.send();
+}
