83c1bd0be3ddc4a66216b6a97ba05abc21d2a6ca
chengquan223 <309581454@qq.com>
2016-05-04 18:30:25 +0800

    add cityclick

--- js/weather.js
+++ js/weather.js
@@ -11,4 +11,18 @@ $(function () {
             var id = $(this).children("a").attr("href").split("#")[1];
             $("#" + id).addClass("active").siblings().removeClass("active");
         });
+        $("#province a").click(function (e) {
+            if ($(this).hasClass("hover")) return false;
+            $("#province a,#city a").removeClass("hover");
+            $(this).addClass("hover");
+            var tabli = $("#tab1 li[class='active']");
+            tabli.children("a").text($(this).text());
+            tabli.siblings().children("a").text("选择城市");
+        });
+        $("#city a").click(function () {
+            if ($(this).hasClass("hover")) return false;
+            $("#city a").removeClass("hover");
+            $(this).addClass("hover");
+            $("#tab1 li[class='active']").children("a").text($(this).text());
+        });
 });
