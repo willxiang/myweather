5c248fd2b47b2f34cb3784a5f26e87a0f5303060
向炜 <willxiang@qq.com>
2016-05-04 10:12:40 +0800

    添加了天气显示界面样式，绑定了天气数据

--- js/my_weather.js
+++ js/my_weather.js
@@ -25,37 +25,44 @@ app.controller('listCtrl', function($scope, $http) {
         citylist = angular.fromJson(localStorage['citylist']);
     }
 
-    // var url = "https://api.heweather.com/x3/weather?cityid=CN101010100&key=6a7d5f6938da427fbb17219e1461e4e4";
-    // $http({
-    //     method: 'GET',
-    //     url: url
-    // }).then(function(response) {
-    //     var weather = response.data;
-    //     console.log(weather['HeWeather data service 3.0'][0]);
-    // }, function(data) {
-    //     console.log(data);
-    // });
+    var url = "https://api.heweather.com/x3/weather?cityid=CN101280101&key=6a7d5f6938da427fbb17219e1461e4e4";
+    $http({
+        method: 'GET',
+        url: url
+    }).then(function(response) {
+        var weather = response.data['HeWeather data service 3.0'][0];
+        weather.daily_forecast.shift();
+        //处理星期几
+        weather = convertDate(weather);
+        weather.nowtime = getDate();
+        $scope.weather = weather;
+        console.log(weather);
+    }, function(data) {
+        console.log(data);
+    });
 
     filterCity(citylist);
 
-    $("#citylist").autocomplete({
-            minLength: 1,
-            source: citylist,
-            //     focus: function(event, ui) {
-            //         $("#citylist").val(ui.item.city);
-            //         return false;
-            //     },
-            select: function(event, ui) {
-                $("#citylist").val(ui.item.desc);
-                $("#cityid").val(ui.item.id);
-                return false;
-            }
-        })
-        .data("ui-autocomplete")._renderItem = function(ul, item) {
-            return $("<li>")
-                .append("<a>" + item.desc + " - " + item.id + "</a>")
-                .appendTo(ul);
-        };
+    // $("#citylist").autocomplete({
+    //         minLength: 1,
+    //         source: citylist,
+    //         //     focus: function(event, ui) {
+    //         //         $("#citylist").val(ui.item.city);
+    //         //         return false;
+    //         //     },
+    //         select: function(event, ui) {
+    //             $("#citylist").val(ui.item.desc);
+    //             $("#cityid").val(ui.item.id);
+    //             localStorage['selectedCityId'] = ui.item.id;
+    //             return false;
+    //         }
+    //     })
+    //     .data("ui-autocomplete")._renderItem = function(ul, item) {
+    //         return $("<li>")
+    //             .append("<a>" + item.desc + " - " + item.id + "</a>")
+    //             .appendTo(ul);
+    //     };
+
 });
 
 
@@ -66,4 +73,23 @@ function filterCity(citylist) {
         citylist[i].label = citylist[i].city;
         citylist[i].desc = citylist[i].city + " - " + citylist[i].prov;
     }
+}
+
+function getDayOfWeek(dayValue) {
+    var day = new Date(Date.parse(dayValue.replace(/-/g, '/'))); // 将日期值格式化
+    var today = new Array("星期天", "星期一", "星期二", "星期三", "星期四", "星期五", "星期六");
+    return today[day.getDay()] //day.getDay(); 根据 Date 返一个星期中的某一天，其中 0 为星期日
+}
+
+function convertDate(weather) {
+    for (var i = 0; i < weather.daily_forecast.length; i++) {
+        weather.daily_forecast[i].weekday = getDayOfWeek(weather.daily_forecast[i].date);
+    }
+    return weather;
+}
+
+function getDate() {
+    var date = new Date();
+    var min = date.getMinutes() > 9 ? date.getMinutes() : "0" + date.getMinutes();
+    return date.getHours() + ":" + min;
 }
