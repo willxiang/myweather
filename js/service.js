2c447ed9e3c23237b89504ce1712ad6f3208a5de
向炜 <willxiang@qq.com>
2016-05-07 22:33:19 +0800

    测试issue

--- js/service.js
+++ js/service.js
@@ -1,10 +1,33 @@
-app.service('httpService', function($http, $q) {
+app.factory('httpService', function($http, $q) {
 
-	// this.setCityId = function()
-	// {
-		
-	// }
+    var service = {};
 
-	// var requestWeatherUrl = "https://api.heweather.com/x3/weather?cityid=" + cityid + "&key=6a7d5f6938da427fbb17219e1461e4e4";
+    var _cityId = '';
+    var weatherUrl = '';
 
-})
+    this.setCityId = function(cityid) {
+        _cityId = cityid;
+    }
+
+    var makeUrl = function() {
+        weatherUrl = "https://api.heweather.com/x3/weather?cityid=" + _cityId + "&key=6a7d5f6938da427fbb17219e1461e4e4";
+    }
+
+    service.getWeather = function() {
+        makeUrl();
+        var defer = $q.defer();
+        $http({
+            method: 'GET',
+            url: weatherUrl
+        }).success(function(data) {
+            defer.resolve(data);
+        }).error(function(data) {
+            defer.reject('There was an error');
+        });
+        return defer.promise;
+    }
+
+
+    return service;
+
+});
